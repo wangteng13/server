7e24ef5a3a33968c3fd81c3a7a866a59d2708b7d
Elena Stepanova <elenst@mariadb.com>
2022-05-13 21:52:20 +0300

    Make 10.6.4 buildable

--- cmake/pcre.cmake
+++ cmake/pcre.cmake
@@ -43,8 +43,8 @@ MACRO(BUNDLE_PCRE2)
   ExternalProject_Add(
     pcre2
     PREFIX   "${dir}"
-    URL      "http://ftp.pcre.org/pub/pcre/pcre2-10.37.zip"
-    URL_MD5  8c1699a725d4b28410adf4b964ebbcb7
+    URL "https://github.com/PhilipHazel/pcre2/releases/download/pcre2-10.40/pcre2-10.40.zip"
+    URL_MD5 798698846982ce171d881ed0d7535c2a
     INSTALL_COMMAND ""
     CMAKE_ARGS
       "-DPCRE2_BUILD_TESTS=OFF"
