0d5568fdb9c203ea76f1f52472dfd165d38f46a7
Vicențiu Ciorbaru <cvicentiu@gmail.com>
2022-06-06 20:37:03 +0300

    cleanup: get_cached_{schema|table}_access is not part of sql_acl.h interface
    The functions are only used internally in the sql_acl.cc module. Mark
    them as static.
    
    TODO: The GRANT_INFO structure inside table could be encapsulated so we do not
          have code that access it like
          table_list->grant.m_internal.<member>

--- sql/sql_acl.cc
+++ sql/sql_acl.cc
@@ -659,6 +659,14 @@ static bool show_table_and_column_privileges(THD *, const char *, const char *,
 static int show_routine_grants(THD *, const char *, const char *,
                                const Sp_handler *sph, char *, int);
 
+static const ACL_internal_schema_access *
+get_cached_schema_access(GRANT_INTERNAL_INFO *grant_internal_info,
+                         const char *schema_name);
+static const ACL_internal_table_access *
+get_cached_table_access(GRANT_INTERNAL_INFO *grant_internal_info,
+                        const char *schema_name,
+                        const char *table_name);
+
 class Grant_tables;
 class User_table;
 class Proxies_priv_table;
@@ -14587,7 +14595,7 @@ ACL_internal_schema_registry::lookup(const char *name)
   @param grant_internal_info the cache
   @param schema_name the name of the internal schema
 */
-const ACL_internal_schema_access *
+static const ACL_internal_schema_access *
 get_cached_schema_access(GRANT_INTERNAL_INFO *grant_internal_info,
                          const char *schema_name)
 {
@@ -14610,7 +14618,7 @@ get_cached_schema_access(GRANT_INTERNAL_INFO *grant_internal_info,
   @param schema_name the name of the internal schema
   @param table_name the name of the internal table
 */
-const ACL_internal_table_access *
+static const ACL_internal_table_access *
 get_cached_table_access(GRANT_INTERNAL_INFO *grant_internal_info,
                         const char *schema_name,
                         const char *table_name)
